0fb7c297714aa5e9307962f937c48bc8aeaf38a7
Mannix Aylward <mannix.aylward@gmail.com>
2023-05-28 16:27:20 +1000

    added cmd line args

--- src/App.java
+++ src/App.java
@@ -3,21 +3,30 @@ import java.net.*;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        //System.out.println("Hello, World!");
-        
-        Transmission transmission = new Transmission();
-        transmission.setIPAddress("192.168.0.20");
-        transmission.setIPPort(6000);
-        transmission.setPagerID("1700150");
-        transmission.setPagerMSG("ABC");
-    
+
+        if (args.length != 4) {
+            System.out.println("Usage: TapOut <server> <port> <pagerID> <message>");
+            System.exit(0);
+        }
         /*
-        System.out.println("IPAdress = " + transmission.getIPAddress());
-        System.out.println("IPPort   = " + transmission.getIPPort());
-        System.out.println("PagerID  = " + transmission.getPagerID());
-        System.out.println("PagerMSG = " + transmission.getPagerMSG());
+        else {
+            try {
+                firstArg = Integer.parseInt(args[0]);
+            } catch (NumberFormatException e) {
+                System.err.println("Argument" + args[0] + " must be an integer.");
+                System.exit(1);
+            }
+        }
         */
-    
+        Transmission transmission = new Transmission();
+        //transmission.setIPAddress("192.168.0.20");
+        //transmission.setIPPort(6000);
+        //transmission.setPagerID("1700150");
+        //transmission.setPagerMSG("ABC");
+        transmission.setIPAddress(args[0]);
+        transmission.setIPPort(Integer.parseInt(args[1]));
+        transmission.setPagerID(args[2]);
+        transmission.setPagerMSG(args[3]);
         transmission.send();
     
         transmission = null;
